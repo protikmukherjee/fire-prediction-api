a4728c306f06d2aa53c190b9d6f5a3458345e5ad
protikmukherjee <protikmukherjee@users.noreply.github.com>
2025-06-27 21:52:26 -0400

    Switched occupancy model to modern Keras format

--- fire_api.py
+++ fire_api.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 # Load models
 fire_model = joblib.load("fire_model_balanced.pkl")
-power_model = tf.keras.models.load_model("power_model.h5")
-occupancy_model = tf.keras.models.load_model("occupancy_model.h5")
+power_model = tf.keras.models.load_model("power_model.keras")
+occupancy_model = tf.keras.models.load_model("occupancy_model.keras")
 
 # Helper for LSTM input formatting
 def prepare_lstm_input(data_dict, feature_order):
